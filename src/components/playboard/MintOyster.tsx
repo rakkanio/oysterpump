8d6cfb1621d50df8e4bc781419b3a8b240b29ab3
abdulbarik <abdulbarikmb@gmail.com>
2024-11-02 22:41:29 +0530

    fix devnet issue

--- src/components/playboard/MintOyster.tsx
+++ src/components/playboard/MintOyster.tsx
@@ -11,7 +11,7 @@ import { useMemo } from "react";
 const sampleNft = new Map([
     [
       "sui:devnet",
-      "0x7e4dc9e4d61f69aa3f07edc58df0ec54298296cdd489c6748f656c8cd72e659f::nft::mint",
+      "0x7e4dc9e4d61f69aa3f07edc58df0ec54298296cdd489c6748f656c8cd72e659f::oyster_game::Oyster",
     ],
     [
       "sui:testnet",
@@ -45,14 +45,14 @@ class MintOyster {
         if (!target) return;
         try {
             const tx = new Transaction();
+            tx.setGasBudget(100000000),
             tx.moveCall({
                 target: target,
                 arguments: [
                     tx.pure.string("Suiet NFT"),
                     tx.pure.string("Suiet Sample NFT"),
-                    tx.pure.string(
-                        "https://xc6fbqjny4wfkgukliockypoutzhcqwjmlw2gigombpp2ynufaxa.arweave.net/uLxQwS3HLFUailocJWHupPJxQsli7aMgzmBe_WG0KC4"
-                    ),
+                    tx.pure.string('0x8'), // Random generator object
+                    tx.pure.string('https://api-mainnet.suifrens.sui.io/suifrens/0x21213e8306d052133b81c5b7d80a11d23f38e4dd2a2c5b26c96c2ef99915a833/svg'),
                 ],
             });
 
