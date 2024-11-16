43ca16929156c91c8039c4737556004b13c699ae
abdulbarik <abdulbarikmb@gmail.com>
2024-11-16 19:11:37 +0530

    update build

--- src/components/playboard/MintOyster.tsx
+++ src/components/playboard/MintOyster.tsx
@@ -11,7 +11,7 @@ import { useMemo } from "react";
 const sampleNft = new Map([
     [
       "sui:devnet",
-      "0x62d7c5354be200ab2f98d2b26624ad4cfe8f7682732b3dac2b6705d2858e5902::oyster_game::hatch_oyster",
+      "0xac52229b1906f493cf9d5ea2b3cd80787adf884a2063ce758be7487c531b221b::oyster_game::hatch_oyster",
     ],
     [
       "sui:testnet",
@@ -51,7 +51,7 @@ class MintOyster {
             }
             if(opts?.isSendStatic){
                 reqArgs = {
-                    target: '0x53931223a6c484af608ff7f853c96066723ca0fc044e7ff92c80053ca5c218af::open_oyster::try_luck',
+                    target: '0xac52229b1906f493cf9d5ea2b3cd80787adf884a2063ce758be7487c531b221b::open_oyster::try_luck',
                     arguments: [
                         tx.pure.string('0x8')   ],
                 }
